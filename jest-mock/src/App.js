1c1e4190e94b3efca773992decd3af985420ca8b
MarlonDValencia <marlondvalencia@gmail.com>
2021-12-12 16:05:05 -0500

    Finished

---
+++ jest-mock/src/App.js
@@ -0,0 +1,14 @@
+import * as math from '../test/math.js';
+export const doAdd = (a, b) => math.add(a, b);
+export const doSubtract = (a, b) => math.subtract(a, b);
+export const doMultiply = (a, b) => math.multiply(a, b);
+export const doDivide = (a, b) => math.divide(a, b);
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
